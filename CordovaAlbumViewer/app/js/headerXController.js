2b57b7477915b089656fb9caf31748b95f560027
Rick Strahl <rstrahl@west-wind.com>
2014-12-24 00:00:47 -1000

    Update sample by optimizing layout for iOS phone.

--- CordovaAlbumViewer/app/js/headerXController.js
+++ CordovaAlbumViewer/app/js/headerXController.js
@@ -3,15 +3,14 @@
 
     angular
         .module('app')
-        .controller('headerController', headerController);        
+        .controller('headerController', headerController);
 
-    console.log('header controller pre');
+    headerController.$inject = ['$scope', '$route', '$window', 'albumServiceLocal'];
 
-    headerController.$inject = ['$scope','$route','$window','albumService'];
-
+    
     function headerController($scope, $route, $window, albumService) {
         console.log('header controller');
-        var vm = $scope;  // straight $scope controller
+        var vm = $scope; // straight $scope controller
 
         vm.searchText = "";
         vm.searchVisible = false;
@@ -19,22 +18,22 @@
         vm.activeTab = albumService.activeTab;
 
         // forward the search key to other controllers
-        vm.onKey = function () {        
+        vm.onKey = function() {
             $scope.$emit('onsearchkey', vm.searchText);
         }
 
-        $scope.searchBlur = function () {            
+        $scope.searchBlur = function() {
             vm.searchText = "search";
         }
 
-        vm.$on("$locationChangeSuccess", function () {            
-            var path = $window.location.hash;
+        vm.$on("$locationChangeSuccess", function() {
+            var path = $window.location.hash;            
             vm.activeTab = path.extract("#/", "/", true);
             isSearchVisible(vm.activeTab);
-            console.log(path, vm.activeTab);            
+            console.log(path, vm.activeTab);
         });
 
-        function isSearchVisible(tab) {            
+        function isSearchVisible(tab) {
             var tab = tab.toLowerCase();
 
             if (tab == "albums" ||
@@ -44,8 +43,9 @@
                 vm.searchVisible = false;
             console.log(vm.searchVisible);
         }
+
         return;
-    }    
+    }
 
 })();
 
--- CordovaHelloWorld/scripts/index.js
+++ CordovaHelloWorld/scripts/index.js
@@ -2,24 +2,25 @@
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
-(function () {
+(function() {
     "use strict";
 
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
-    function onDeviceReady() {        
+    function onDeviceReady() {
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
-               
-        console.log("Device ready...");  
+
+        console.log("Device ready...");
+        console.log(navigator.userAgent);
         start(); // User Code
     };
 
-    function onPause() {        
+    function onPause() {
         console.log("Application paused...");
     };
 
-    function onResume() {        
+    function onResume() {
         console.log("application resumed...");
     };
 
@@ -28,7 +29,9 @@
         var message = document.getElementById("Message");
 
         name.addEventListener("keyup", function() {
+            var ua = navigator.userAgent;
+            console.log(ua);
             message.innerText = name.value;
         });
-    }       
-} )();
+    }
+})();
